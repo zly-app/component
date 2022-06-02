fea54f9192e6b91e5b63f92ca0e0bada6bff4cc1
zlyuanzhang <zlyuanzhang@tencent.com>
2022-06-02 17:19:26 +0800

    简化示例项目

--- grpc-client/example/grpc-c/main.go
+++ grpc-client/example/grpc-c/main.go
@@ -2,46 +2,26 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/zly-app/zapp"
-	"github.com/zly-app/zapp/core"
 
 	grpc_client "github.com/zly-app/component/grpc-client"
 
 	"github.com/zly-app/component/grpc-client/example/grpc-c/pb/hello"
 )
 
-type Component struct {
-	core.IComponent
-	grpc_client.IGrpcClient
-}
-
-func (c *Component) Close() {
-	c.IComponent.Close()
-	c.IGrpcClient.Close()
-}
-
 func main() {
-	app := zapp.NewApp("grpc-c",
-		zapp.WithCustomComponent(func(app core.IApp) core.IComponent { // 自定义组件
-			return &Component{
-				IComponent:  app.GetComponent(),
-				IGrpcClient: grpc_client.NewGrpcClient(app), // 创建grpc客户端
-			}
-		}))
+	app := zapp.NewApp("grpc-c")
 	defer app.Exit()
 
-	c := app.GetComponent().(*Component)
-	// 注册客户端创造者
-	c.RegistryGrpcClientCreator("hello", hello.NewHelloServiceClient)
-	// 获取客户端
-	httpClient := c.GetGrpcClient("hello").(hello.HelloServiceClient)
+	c := grpc_client.NewGrpcClient(app)                               // 获取grpc客户端插件
+	c.RegistryGrpcClientCreator("hello", hello.NewHelloServiceClient) // 注册客户端创造者
+	client := c.GetGrpcClient("hello").(hello.HelloServiceClient)     // 获取客户端
 
 	// 调用
-	resp, err := httpClient.Hello(context.Background(), &hello.HelloReq{Msg: "hello"})
+	resp, err := client.Hello(context.Background(), &hello.HelloReq{Msg: "hello"})
 	if err != nil {
 		app.Fatal(resp)
 	}
-	fmt.Println(resp.GetMsg())
+	app.Info("收到结果", resp.GetMsg())
 }
