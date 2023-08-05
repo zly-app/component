86e87a07139186fcbf4419ac519dc019f631cf8e
zlyuancn <zlyuancn@163.com>
2023-08-05 13:49:32 +0800

    添加link和util

--- mongo/link.go
+++ mongo/link.go
@@ -1,7 +1,46 @@
 package mongo
 
 import (
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Client = mongo.Client
+type (
+	Client     = mongo.Client
+	Database   = mongo.Database
+	Collection = mongo.Collection
+	Cursor     = mongo.Cursor
+
+	SessionContext = mongo.SessionContext
+	Session = mongo.Session
+)
+
+var (
+	ErrUnacknowledgedWrite = mongo.ErrUnacknowledgedWrite
+	ErrClientDisconnected  = mongo.ErrClientDisconnected
+	ErrNilDocument         = mongo.ErrNilDocument
+	ErrNilValue            = mongo.ErrNilValue
+	ErrEmptySlice          = mongo.ErrEmptySlice
+
+	// 来自服务器的更改流通知不包含恢复令牌
+	ErrMissingResumeToken = mongo.ErrMissingResumeToken
+	// 更改流的底层游标为nil
+	ErrNilCursor = mongo.ErrNilCursor
+
+	// 使用具有非数字或字符串值的键文档创建索引
+	ErrInvalidIndexValue = mongo.ErrInvalidIndexValue
+	// 创建索引的名称不是字符串
+	ErrNonStringIndexName = mongo.ErrNonStringIndexName
+	// 多个索引将被删除
+	ErrMultipleIndexDrop = mongo.ErrMultipleIndexDrop
+
+	// 该客户端没有创建会话
+	ErrWrongClient = mongo.ErrWrongClient
+)
+
+type (
+	BsonD = bson.D
+	BsonE = bson.E
+	BsonM = bson.M
+	BsonA = bson.A
+)
---
+++ mongo/util.go
@@ -0,0 +1,64 @@
+package mongo
+
+import (
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// 构建用于排序的bson
+// 示例: sort := MakeSortBson("level", "-create_time")
+func MakeSortBson(fields ...string) bson.D {
+	var order bson.D
+	for _, field := range fields {
+		n := 1
+		var kind string
+		if field != "" {
+			if field[0] == '$' {
+				if c := strings.Index(field, ":"); c > 1 && c < len(field)-1 {
+					kind = field[1:c]
+					field = field[c+1:]
+				}
+			}
+			switch field[0] {
+			case '+':
+				field = field[1:]
+			case '-':
+				n = -1
+				field = field[1:]
+			}
+		}
+		if field == "" {
+			panic("Sort: empty field name")
+		}
+		if kind == "textScore" {
+			order = append(order, bson.E{field, bson.M{"$meta": kind}})
+		} else {
+			order = append(order, bson.E{field, n})
+		}
+	}
+	return order
+}
+
+// 构建用于选择返回字段的bson
+// 示例: project := MakeSelectBson("-_id", "create_time")
+func MakeSelectBson(fields ...string) bson.M {
+	project := make(bson.M, len(fields))
+	for _, field := range fields {
+		n := 1
+		if field != "" {
+			switch field[0] {
+			case '+':
+				field = field[1:]
+			case '-':
+				n = 0
+				field = field[1:]
+			}
+		}
+		if field == "" {
+			panic("Project: empty field name")
+		}
+		project[field] = n
+	}
+	return project
+}
